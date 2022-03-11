35e07ebbef8d59fecb2885c218e3c77b06130575
Matthew Reishus <mreishus@users.noreply.github.com>
2022-03-11 09:37:04 -0600

    Add tests and small refactor

--- cube.go
+++ cube.go
@@ -39,18 +39,18 @@ const (
 // {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23} // index
 // {A, B, C, D, E, F, G, H, I, J,  K,  L,  M,  N,  O,  P,  Q,  R,  S,  T,  U,  V,  W,  X} // blindfold
 
-func do_turn(cube [24]CubeColor, turn CubeTurn) [24]CubeColor {
-	var turns [3][4]int
+func doTurn(cube [24]CubeColor, turn CubeTurn) [24]CubeColor {
+	var turns [][4]int
 
 	switch turn {
 	case U:
-		turns = [3][4]int{
+		turns = [][4]int{
 			{8, 12, 16, 4}, // I M Q E
 			{9, 13, 17, 5}, // J N R F
 			{0, 3, 2, 1},   // A D C B
 		}
 	case UP:
-		turns = [3][4]int{
+		turns = [][4]int{
 			{8, 4, 16, 12}, // I E Q M
 			{9, 5, 17, 13}, // J F R N
 			{0, 1, 2, 3},   // A B C D
@@ -61,27 +61,36 @@ func do_turn(cube [24]CubeColor, turn CubeTurn) [24]CubeColor {
 		// Swap J,R
 		// Swap E,M
 		// Swap F,N
+		// Also rotate the U face
 	case R:
-		turns = [3][4]int{
+		turns = [][4]int{
 			{9, 21, 19, 1},   // J V T B
 			{10, 22, 16, 2},  // K W Q C
 			{12, 15, 14, 13}, // M P O N
 		}
 	case RP:
-		turns = [3][4]int{
+		turns = [][4]int{
 			{9, 1, 19, 21},   // J B T V
 			{10, 2, 16, 22},  // K C Q W
 			{12, 13, 14, 15}, // M N O P
 		}
+	case R2:
+		// 4 style turns don't work, I need to do direct swaps...
+		// Also rotate the R face
+	case F:
+	case FP:
+	case F2:
+		// 4 style turns don't work, I need to do direct swaps...
+		// Also rotate the F face
 	}
 
-	cube = do_rotate(cube, turns[0])
-	cube = do_rotate(cube, turns[1])
-	cube = do_rotate(cube, turns[2])
+	for _, turn := range turns {
+		cube = doRotate(cube, turn)
+	}
 	return cube
 }
 
-func do_rotate(cube [24]CubeColor, is [4]int) [24]CubeColor {
+func doRotate(cube [24]CubeColor, is [4]int) [24]CubeColor {
 	var temp CubeColor
 	a, b, c, d := is[0], is[1], is[2], is[3]
 	temp = cube[a]
@@ -95,34 +104,34 @@ func do_rotate(cube [24]CubeColor, is [4]int) [24]CubeColor {
 func display(cube [24]CubeColor) {
 	// Print U
 	fmt.Print("    ")
-	display_squares(cube, []int{0, 1})
+	displaySquares(cube, []int{0, 1})
 	fmt.Println()
 	fmt.Print("    ")
-	display_squares(cube, []int{3, 2})
+	displaySquares(cube, []int{3, 2})
 	fmt.Println()
 	// Print L, F, R, B
-	display_squares(cube, []int{4, 5, 8, 9, 12, 13, 16, 17})
+	displaySquares(cube, []int{4, 5, 8, 9, 12, 13, 16, 17})
 	fmt.Println()
-	display_squares(cube, []int{7, 6, 11, 10, 15, 14, 19, 18})
+	displaySquares(cube, []int{7, 6, 11, 10, 15, 14, 19, 18})
 	fmt.Println()
 	// Print D
 	fmt.Print("    ")
-	display_squares(cube, []int{20, 21})
+	displaySquares(cube, []int{20, 21})
 	fmt.Println()
 	fmt.Print("    ")
-	display_squares(cube, []int{23, 22})
+	displaySquares(cube, []int{23, 22})
 	fmt.Println()
 	fmt.Println()
 }
 
-func display_squares(cube [24]CubeColor, indexes []int) {
+func displaySquares(cube [24]CubeColor, indexes []int) {
 	for _, x := range indexes {
-		display_square(cube[x])
-		display_square(cube[x])
+		displaySquare(cube[x])
+		displaySquare(cube[x])
 	}
 }
 
-func display_square(cc CubeColor) {
+func displaySquare(cc CubeColor) {
 	block := "█"
 	switch cc {
 	case White:
@@ -140,41 +149,33 @@ func display_square(cc CubeColor) {
 	}
 }
 
-func get_solved_cube() [24]CubeColor {
+func GetSolvedCube() [24]CubeColor {
 	cube := [24]CubeColor{White, White, White, White, Orange, Orange, Orange, Orange, Green, Green, Green, Green, Red, Red, Red, Red, Blue, Blue, Blue, Blue, Yellow, Yellow, Yellow, Yellow}
 	return cube
 }
 
-func smove(cube [24]CubeColor) [24]CubeColor {
-	cube = do_turn(cube, R)
-	cube = do_turn(cube, U)
-	cube = do_turn(cube, RP)
-	cube = do_turn(cube, UP)
+func SMove(cube [24]CubeColor) [24]CubeColor {
+	cube = doTurn(cube, R)
+	cube = doTurn(cube, U)
+	cube = doTurn(cube, RP)
+	cube = doTurn(cube, UP)
 	// display(cube)
 	return cube
 }
 
 func main() {
-	cube := get_solved_cube()
+	cube := GetSolvedCube()
 	display(cube)
-	cube = smove(cube)
+	cube = SMove(cube)
 	display(cube)
-	cube = smove(cube)
+	cube = SMove(cube)
 	display(cube)
-	cube = smove(cube)
+	cube = SMove(cube)
 	display(cube)
-	cube = smove(cube)
+	cube = SMove(cube)
 	display(cube)
-	cube = smove(cube)
+	cube = SMove(cube)
 	display(cube)
-	cube = smove(cube)
+	cube = SMove(cube)
 	display(cube)
-	// cube = do_turn(cube, U)
-	// display(cube)
-	// cube = do_turn(cube, UP)
-	// display(cube)
-	// cube = do_turn(cube, UP)
-	// display(cube)
-	// cube = do_turn(cube, RP)
-	// display(cube)
 }
--- cube_test.go
+++ cube_test.go
@@ -2,10 +2,33 @@ package main
 
 import "testing"
 
-func TestAddOne(t *testing.T) {
-	t.Run("It adds one", func(t *testing.T) {
-		got := AddOne(1)
-		want := 2
+func TestSixSMoves(t *testing.T) {
+	t.Run("6 SMoves Restores the Cube", func(t *testing.T) {
+		cube1 := GetSolvedCube()
+		for i := 0; i < 6; i += 1 {
+			cube1 = SMove(cube1)
+		}
+		got := cube1
+		want := GetSolvedCube()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+}
+
+func TestTwoSMoves(t *testing.T) {
+	t.Run("2 SMoves", func(t *testing.T) {
+		cube1 := GetSolvedCube()
+		for i := 0; i < 2; i += 1 {
+			cube1 = SMove(cube1)
+		}
+		got := cube1
+		want := [24]CubeColor{Orange, Blue, Green, White, // U
+			Blue, Orange, Orange, Orange, // L
+			Green, Red, Yellow, Green, // F
+			White, White, Red, Green, // R
+			Red, White, Blue, Blue,  // B
+			Yellow, Red, Yellow, Yellow} // D
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
 		}
